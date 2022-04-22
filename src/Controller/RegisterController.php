97254097e5b83a9cd37612b29c8cd568f0535922
Dongmo880 <84510058+Dongmo880@users.noreply.github.com>
2022-04-22 11:36:13 +0200

    Creation de nos premiéres entité
    -creation du formulaire et validation des informations en BDD
    -ajout des contraintes de validation
    -ajout des messages flash

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(EntityManagerInterface  $em, Request $request,UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('sucsess','Inscription resussite');
+            return $this->redirectToRoute('app_home');
+
+
+        }
+        return $this->render('register/register.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+}
